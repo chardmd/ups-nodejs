703150b46d09d14c195919246bdda4e15d794f8a
Richard Dimalanta <richard.dimalanta@gmail.com>
2013-12-15 15:10:54 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var ShipConfirm = require('./lib/shipConfirm');
+var ShipAccept = require('./lib/shipAccept');
+var AddressValidation = require('./lib/AddressValidation');
+var VoidShipment = require('./lib/voidShipment');
+var TimeInTransit = require('./lib/timeInTransit');
+
